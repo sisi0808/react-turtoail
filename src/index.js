85f2ec3585ba2d8205e52a7a8c138d480986fbe7
sisi0808 <sivawqoqpqwu@gmail.com>
2022-06-10 14:06:09 +0900

    #1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
+import * as ReactDOMClient from "react-dom/client";
 import "./index.css";
 
 class Square extends React.Component {
@@ -16,25 +17,28 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
-  renderSquare(i) {
+  renderSquare(i, j) {
+    // console.log(this.props.winLines);
     return (
       <Square
-        value={this.props.squares[i]}
-        onClick={() => this.props.handleClick(i)}
-        key={"square-" + i.toString()}
-        isHilight={this.props.winLines.includes(i)}
+        value={this.props.squares[i][j]}
+        onClick={() => this.props.handleClick(i, j)}
+        key={`Square-(${i}, ${j})`}
+        isHilight={this.props.winLines.some((wl) => {
+          return i === wl[0] && j === wl[1];
+        })}
       />
     );
   }
 
   createSquares() {
     let squares = [];
-    const row = 3;
     const col = 3;
+    const row = 3;
     for (let i = 0; i < col; i++) {
       let rows = [];
       for (let j = 0; j < row; j++) {
-        rows.push(this.renderSquare(i * 3 + j));
+        rows.push(this.renderSquare(i, j));
       }
       squares.push(
         <div className="board-row" key={"rows" + i.toString()}>
@@ -56,7 +60,14 @@ class Game extends React.Component {
     this.state = {
       history: [
         {
-          squares: Array(9).fill(null),
+          // squares: Array(3)
+          //   .fill()
+          //   .map(() => Array(3).fill(null)),
+          squares: [
+            [null, null, null],
+            [null, null, null],
+            [null, null, null],
+          ],
           col: null,
           row: null,
         },
@@ -67,24 +78,29 @@ class Game extends React.Component {
     };
   }
 
-  handleClick(i) {
+  handleClick(i, j) {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
-    const squares = current.squares.slice();
-    const col = Math.floor(i / 3) + 1;
-    const row = (i % 3) + 1;
+    const squares = current.squares.map((squares2) => squares2.slice()).slice();
 
-    if (calculateWinner(squares)[0] || squares[i]) {
+    if (calculateWinner(squares)[0] || squares[i][j]) {
       return;
     }
-    squares[i] = this.state.xIsNext ? "X" : "O";
+    // console.log(history[0]);
+    squares[i][j] = this.state.xIsNext ? "X" : "O";
+    // console.log(
+    //   history[this.state.stepNumber - 1].col,
+    //   history[this.state.stepNumber - 1].row
+    // );
+    // console.log(history.length);
+    // console.log(history[0]);
 
     this.setState({
       history: history.concat([
         {
-          squares,
-          col,
-          row,
+          squares: squares,
+          col: i,
+          row: j,
         },
       ]),
       stepNumber: history.length,
@@ -102,6 +118,8 @@ class Game extends React.Component {
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
+    // console.log(this.state.history);
+    // console.log("fd");
     const winner = calculateWinner(current.squares);
 
     /* ここでトグルボタンに応じたソートを行う */
@@ -144,7 +162,7 @@ class Game extends React.Component {
         <div className="game-board">
           <Board
             squares={current.squares}
-            handleClick={(i) => this.handleClick(i)}
+            handleClick={(i, j) => this.handleClick(i, j)}
             winLines={winLines}
           />
         </div>
@@ -168,29 +186,104 @@ class Game extends React.Component {
 
 // ========================================
 //
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOMClient.createRoot(document.getElementById("root"));
 root.render(<Game />);
 
 function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
+  const row = 3;
+  const col = 3;
+
   let winner = null;
   let winLines = [];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      winner = squares[a];
-      winLines = winLines.concat(lines[i]);
+  // const lines = [
+  //   [0, 1, 2],
+  //   [3, 4, 5],
+  //   [6, 7, 8],
+  //   [0, 3, 6],
+  //   [1, 4, 7],
+  //   [2, 5, 8],
+  //   [0, 4, 8],
+  //   [2, 4, 6],
+  // ];
+  // for (let i = 0; i < lines.length; i++) {
+  //   const [a, b, c] = lines[i];
+  //   if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+  //     winner = squares[a];
+  //     winLines = winLines.concat(lines[i]);
+  //   }
+  // }
+
+  /* 縦横揃った場合を調べる */
+  let isAlignHorizontal = Array(col).fill(true);
+  let isAlignVertical = Array(row).fill(true);
+  for (let i = 0; i < col; i++) {
+    for (let j = 0; j < row; j++) {
+      /* 横のラインが揃った場合 */
+      if (squares[i][j] == null || squares[i][j] !== squares[i][0])
+        isAlignHorizontal[i] = false;
+      /* 縦のラインが揃った場合 */
+      if (squares[i][j] == null || squares[i][j] !== squares[0][j])
+        isAlignVertical[j] = false;
     }
   }
-  if (!squares.includes(null) && winner === null) return ["Draw", null];
+
+  /* 斜めに揃った場合 */
+  let isAlignDiagonal = Array(2).fill(true);
+  for (let i = 0; i < col; i++) {
+    /* 左下-右上のラインがに揃った場合 */
+    if (squares[i][i] == null || squares[i][i] !== squares[0][0])
+      isAlignDiagonal[0] = false;
+    /* 右下-左上のラインがに揃った場合 */
+    if (
+      squares[i][row - i - 1] == null ||
+      squares[i][row - i - 1] !== squares[0][row - 1]
+    )
+      isAlignDiagonal[1] = false;
+  }
+
+  /* 横に揃った場合 */
+  for (let i = 0; i < col; i++) {
+    if (isAlignHorizontal[i]) {
+      winner = squares[i][0];
+      winLines = winLines.concat(
+        Array(row)
+          .fill(null)
+          .map((_, idx) => [i, idx])
+      );
+    }
+  }
+
+  /* 縦に揃った場合 */
+  for (let j = 0; j < row; j++) {
+    if (isAlignVertical[j]) {
+      winner = squares[0][j];
+      winLines = winLines.concat(
+        Array(col)
+          .fill(null)
+          .map((_, idx) => [idx, j])
+      );
+    }
+  }
+
+  /* 斜めに揃った場合 */
+  if (isAlignDiagonal[0]) {
+    winner = squares[0][0];
+    winLines = winLines.concat(
+      Array(col)
+        .fill(null)
+        .map((_, idx) => [idx, idx])
+    );
+  }
+  if (isAlignDiagonal[1]) {
+    winner = squares[0][row - 1];
+    winLines = winLines.concat(
+      Array(col)
+        .fill(null)
+        .map((_, idx) => [idx, row - idx - 1])
+    );
+  }
+
+  if (!squares.some((squares2) => squares2.includes(null)) && winner === null)
+    return ["Draw", null];
   return [winner, winLines];
 }
